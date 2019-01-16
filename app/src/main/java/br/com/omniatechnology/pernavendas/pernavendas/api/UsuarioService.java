cded3c2c261144447c784427e70f49bd82fe4588
legionario07 <paulinho.legionario@hotmail.com>
2019-01-15 22:37:35 -0200

    Criacao da Interface UsuarioService

--- app/src/main/java/br/com/omniatechnology/pernavendas/pernavendas/api/UsuarioService.java
+++ app/src/main/java/br/com/omniatechnology/pernavendas/pernavendas/api/UsuarioService.java
@@ -1,5 +1,8 @@
 package br.com.omniatechnology.pernavendas.pernavendas.api;
 
+import java.util.List;
+
+import br.com.omniatechnology.pernavendas.pernavendas.model.Usuario;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
@@ -17,15 +20,18 @@ public interface UsuarioService {
 
 
     @GET(BASE_URL_USUARIOS)
-    Call<List<Bounty>> findAll();
+    Call<List<Usuario>> findAll();
 
     @GET(FIND_BY_ID)
-    Call<Bounty> findById(@Query("id") Integer id);
+    Call<Usuario> findById(@Query("id") Integer id);
 
     @POST(SAVE)
-    Call<Bounty> save(@Body Bounty local);
+    Call<Usuario> save(@Body Usuario usuario);
 
     @DELETE(DELETE)
     Call<Boolean> delete(@Query("id") Integer id);
+
+    @POST(LOGIN)
+    Call<Usuario> login(@Body Usuario usuario);
 
 }

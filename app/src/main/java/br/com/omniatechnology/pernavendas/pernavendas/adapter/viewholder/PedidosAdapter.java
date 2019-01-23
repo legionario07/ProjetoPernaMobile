287e2ef8358e04fca5893c59ec2c35b05d410edd
legionario07 <paulinho.legionario@hotmail.com>
2019-01-23 12:12:50 -0200

    Criado a lógica de vendas

---
+++ app/src/main/java/br/com/omniatechnology/pernavendas/pernavendas/adapter/viewholder/PedidosAdapter.java
@@ -0,0 +1,54 @@
+package br.com.omniatechnology.pernavendas.pernavendas.adapter.viewholder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.omniatechnology.pernavendas.pernavendas.R;
+import br.com.omniatechnology.pernavendas.pernavendas.model.Pedido;
+
+
+/**
+ * Created by PauLinHo on 10/08/2017.
+ */
+
+public class PedidosAdapter extends ArrayAdapter<Pedido> {
+
+    private Context context;
+    private List<Pedido> lista;
+
+    public PedidosAdapter(Context context, List<Pedido> lista) {
+        super(context, 0, lista);
+        this.context = context;
+        this.lista = new ArrayList<>();
+        this.lista = lista;
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Pedido pedido = this.lista.get(position);
+
+        convertView = LayoutInflater.from(this.context).inflate(R.layout.item_pedido, null);
+
+        TextView txtItemNomeProduto = (TextView) convertView.findViewById(R.id.txtItemNomeProduto);
+        TextView txtItemQtdeProduto = (TextView) convertView.findViewById(R.id.txtItemQtdeProduto);
+        TextView txtItemDesconto = (TextView) convertView.findViewById(R.id.txtItemDesconto);
+        TextView txtItemValor = (TextView) convertView.findViewById(R.id.txtItemValor);
+
+        txtItemNomeProduto.setText(pedido.getProduto().getNome());
+        txtItemQtdeProduto.setText(pedido.getTotal().toString());
+        txtItemDesconto.setText(pedido.getDesconto().toString());
+        txtItemValor.setText(pedido.getValorTotal().toString());
+
+        return convertView;
+    }
+
+}
--- app/src/main/java/br/com/omniatechnology/pernavendas/pernavendas/telas/NewVendaActivity.java
+++ app/src/main/java/br/com/omniatechnology/pernavendas/pernavendas/telas/NewVendaActivity.java
@@ -1,21 +1,27 @@
 package br.com.omniatechnology.pernavendas.pernavendas.telas;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,8 +31,10 @@ import br.com.omniatechnology.pernavendas.pernavendas.Presenter.VendaPresenter;
 import br.com.omniatechnology.pernavendas.pernavendas.R;
 import br.com.omniatechnology.pernavendas.pernavendas.View.IModelView;
 import br.com.omniatechnology.pernavendas.pernavendas.adapter.MarcasAdapter;
+import br.com.omniatechnology.pernavendas.pernavendas.adapter.viewholder.PedidosAdapter;
 import br.com.omniatechnology.pernavendas.pernavendas.api.impl.GenericDAO;
 import br.com.omniatechnology.pernavendas.pernavendas.model.IModel;
+import br.com.omniatechnology.pernavendas.pernavendas.model.Pedido;
 import br.com.omniatechnology.pernavendas.pernavendas.model.Produto;
 import br.com.omniatechnology.pernavendas.pernavendas.utils.ConstraintUtils;
 
@@ -35,14 +43,27 @@ import static android.widget.Toast.LENGTH_LONG;
 public class NewVendaActivity extends AppCompatActivity implements IModelView.IVendaView, View.OnClickListener {
 
     private ListView lstPedidos;
+    private List<Pedido> pedidos;
     private MarcasAdapter marcasAdapter;
     private AutoCompleteTextView inpProduto;
     IVendaPresenter vendaPresenter;
     private ImageButton imgAdicionarProduto;
     private Produto produto;
+    private TextView txtTotal;
 
+    private PedidosAdapter pedidosAdapter;
+
+    private LayoutInflater inflater;
+    private AlertDialog alert;
+    private AlertDialog dialog;
+    private AlertDialog.Builder dialogBuilder;
 
     private ProgressDialog progressDialog;
+
+    private TextView txtNomeProduto;
+    private TextView txtDescricaoProduto;
+    private TextInputLayout inpLayoutQuantidade;
+    private TextInputLayout inpLayoutDesconto;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -51,45 +72,84 @@ public class NewVendaActivity extends AppCompatActivity implements IModelView.IV
 
         lstPedidos = findViewById(R.id.lstPedidos);
         inpProduto = findViewById(R.id.inp_produto);
+        txtTotal  =findViewById(R.id.txtTotal);
         imgAdicionarProduto = findViewById(R.id.imgAdicionarProduto);
-
 
 
         imgAdicionarProduto.setOnClickListener(this);
 
 
-//        List<Produto> produtos = new ArrayList<>();
-//        Produto p = new Produto();
-//        p.setNome("CASA COM PISCINA");
-//        p.setQtde(2);
-//
-//        Produto p3 = new Produto();
-//        p3.setNome("CASA SEM PISCINA");
-//        p3.setQtde(2);
-//
-//        Produto p2 = new Produto();
-//        p2.setNome("CArro COM PISCINA");
-//        p2.setQtde(2);
-//
-//        produtos.add(p);
-//        produtos.add(p2);
-//        produtos.add(p3);
+        final List<Produto> produtos = new ArrayList<>();
+        Produto p = new Produto();
+        p.setNome("CASA COM PISCINA");
+        p.setDescricao("Casa");
+        p.setQtde(2);
+        p.setValorVenda(new BigDecimal("10.00"));
+
+        Produto p3 = new Produto();
+        p3.setNome("CASA SEM PISCINA");
+        p3.setDescricao("Casa");
+        p3.setQtde(2);
+        p3.setValorVenda(new BigDecimal("10.00"));
+
+        Produto p2 = new Produto();
+        p2.setNome("CArro COM PISCINA");
+        p2.setQtde(2);
+        p2.setDescricao("CARRO");
+        p2.setValorVenda(new BigDecimal("10.00"));
+
+        produtos.add(p);
+        produtos.add(p2);
+        produtos.add(p3);
 
 
         vendaPresenter = new VendaPresenter(this, this);
        // ((MarcaPresenter) marcaPresenter).initialize(rcViewMarcas);
 
+        final ArrayAdapter<Produto> adapter = new ArrayAdapter<Produto>
+                (this, android.R.layout.select_dialog_item, produtos);
+
+        inpProduto.setAdapter(adapter);
+        inpProduto.setThreshold(1);
+        inpProduto.setTextColor(Color.RED);
 
 
-        ((VendaPresenter) vendaPresenter).addDataForAdapter(inpProduto);
+        //((VendaPresenter) vendaPresenter).addDataForAdapter(inpProduto);
 
         inpProduto.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                produto = (Produto) parent.getSelectedItem();
+                produto = adapter.getItem(position);
+
             }
         });
 
+        atualizarListDePedidos();
+
+
+    }
+
+
+    public void atualizarListDePedidos(){
+
+        BigDecimal valorTotal = new BigDecimal("0.0");
+
+        if(pedidosAdapter==null){
+            pedidos = new ArrayList<>();
+            pedidosAdapter = new PedidosAdapter(this, pedidos);
+            lstPedidos.setAdapter(pedidosAdapter);
+
+        }else{
+
+            pedidosAdapter.notifyDataSetChanged();
+
+            for(Pedido p : pedidos){
+                valorTotal = valorTotal.add(p.getValorTotal());
+            }
+
+        }
+
+        txtTotal.setText(valorTotal.toString());
 
 
     }
@@ -137,6 +197,8 @@ public class NewVendaActivity extends AppCompatActivity implements IModelView.IV
                    Toast.makeText(this, getString(R.string.selecione_produto_primeiro), Toast.LENGTH_LONG).show();
                }else{
                    //Regra para abrir dialog com o produto selecionado
+                   Toast.makeText(this, produto.getNome(), Toast.LENGTH_LONG).show();
+                   criarDialogCRUD("SALVAR");
                }
 
                 break;
@@ -145,6 +207,62 @@ public class NewVendaActivity extends AppCompatActivity implements IModelView.IV
         }
 
     }
+
+    private void criarDialogCRUD(final String operacao) {
+
+        dialogBuilder = new AlertDialog.Builder(this);
+
+        inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.new_pedido, null);
+        dialogBuilder.setView(dialogView);
+
+        txtNomeProduto = dialogView.findViewById(R.id.txtNomeProduto);
+        txtDescricaoProduto = dialogView.findViewById(R.id.txtDescricaoProduto);
+        inpLayoutQuantidade = dialogView.findViewById(R.id.inp_layout_quantidade);
+        inpLayoutDesconto = dialogView.findViewById(R.id.inp_layout_desconto);
+
+        txtNomeProduto.setText(produto.getNome());
+        txtDescricaoProduto.setText(produto.getDescricao());
+        inpLayoutQuantidade.getEditText().setText("1");
+        inpLayoutDesconto.getEditText().setText("0");
+
+        dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Pedido pedido = new Pedido();
+                pedido.setProduto(produto);
+                pedido.setDesconto(new BigDecimal(inpLayoutDesconto.getEditText().getText().toString()));
+                pedido.setTotal(Integer.valueOf(inpLayoutQuantidade.getEditText().getText().toString()));
+
+                BigDecimal valor = pedido.getProduto().getValorVenda().multiply(new BigDecimal(pedido.getTotal()));
+                valor = valor.subtract(pedido.getDesconto());
+                pedido.setValorTotal(valor);
+
+                pedidos.add(pedido);
+
+                atualizarListDePedidos();
+
+
+
+            }
+        })
+
+
+                .setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                        return;
+
+                    }
+                });
+
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+    }
+
 
 
 

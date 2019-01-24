cd51afe7a1a60aef5d584a8832be1b878462ba0a
legionario07 <paulinho.legionario@hotmail.com>
2019-01-24 15:40:04 -0200

    Criado Salvar senha no metodo de Login

--- app/src/main/java/br/com/omniatechnology/pernavendas/pernavendas/telas/LoginActivity.java
+++ app/src/main/java/br/com/omniatechnology/pernavendas/pernavendas/telas/LoginActivity.java
@@ -1,22 +1,27 @@
 package br.com.omniatechnology.pernavendas.pernavendas.telas;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import br.com.omniatechnology.pernavendas.pernavendas.R;
 import br.com.omniatechnology.pernavendas.pernavendas.View.ILoginView;
 import br.com.omniatechnology.pernavendas.pernavendas.Presenter.ILoginPresenter;
 import br.com.omniatechnology.pernavendas.pernavendas.Presenter.LoginPresenter;
+import br.com.omniatechnology.pernavendas.pernavendas.utils.ConstraintUtils;
 
 public class LoginActivity extends AppCompatActivity implements ILoginView, View.OnClickListener {
 
 
     Button btn_login;
+    CheckBox chkSalvarSenha;
+    private Boolean isChecked;
 
     TextInputLayout usuarioWrapper;
     TextInputLayout senhaWrapper;
@@ -29,9 +34,12 @@ public class LoginActivity extends AppCompatActivity implements ILoginView, View
         setContentView(R.layout.login_activity);
 
         btn_login = findViewById(R.id.btn_login);
+        chkSalvarSenha = findViewById(R.id.chkSalvarSenha);
 
         usuarioWrapper = findViewById(R.id.usuarioWrapper);
         senhaWrapper = findViewById(R.id.senhaWrapper);
+
+        verificarDadosSalvo();
 
         loginPresenter = new LoginPresenter(this, this);
         ((LoginPresenter) loginPresenter).addSenhaTextWatcher(usuarioWrapper.getEditText());
@@ -41,8 +49,35 @@ public class LoginActivity extends AppCompatActivity implements ILoginView, View
 
     }
 
+    private void verificarDadosSalvo(){
+
+        SharedPreferences prefs = getSharedPreferences(ConstraintUtils.PREFERENCES, MODE_PRIVATE);
+        String login = prefs.getString("login", null);
+        String senha = prefs.getString("senha", null);
+        isChecked = prefs.getBoolean("checked", false);
+
+        if (login != null && isChecked) {
+            usuarioWrapper.getEditText().setText(login);
+            senhaWrapper.getEditText().setText(senha);
+            chkSalvarSenha.setChecked(isChecked);
+        }
+
+    }
+
+
+    private void atualizarDadosSalvo(){
+
+        SharedPreferences.Editor editor = getSharedPreferences(ConstraintUtils.PREFERENCES, MODE_PRIVATE).edit();
+        editor.putString("login", usuarioWrapper.getEditText().getText().toString());
+        editor.putString("senha", senhaWrapper.getEditText().getText().toString());
+        editor.putBoolean("checked", chkSalvarSenha.isChecked());
+        editor.commit();
+
+    }
+
     @Override
     public void OnLoginResultSuccess() {
+        atualizarDadosSalvo();
         Toast.makeText(this, getResources().getString(R.string.login_sucess),Toast.LENGTH_LONG).show();
         startActivity(new Intent(this, HomeActivity.class));
 
--- app/src/main/java/br/com/omniatechnology/pernavendas/pernavendas/utils/ConstraintUtils.java
+++ app/src/main/java/br/com/omniatechnology/pernavendas/pernavendas/utils/ConstraintUtils.java
@@ -20,4 +20,5 @@ public class ConstraintUtils {
     public static final String CONFIGURACAO_INTENT = "0001";
     public static final String UNIDADE_DE_MEDIDA_INTENT = "0001";
     public static final String USUARIO_INTENT = "0001";
+    public static final String PREFERENCES = "PREFERENCES_APP";
 }
